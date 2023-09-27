94f3f1e04c091fd2a2871e975e196cead526f07f
Dan Johns <danjhd@amazon.com>
2023-09-27 09:20:16 +0100

    updated GP3 function logic

--- rules/Ebs-Gp3/function.py
+++ rules/Ebs-Gp3/function.py
@@ -1,13 +1,10 @@
 def evaluate_compliance(configuration_item, valid_rule_parameters):
-    if configuration_item["resourceType"] != "AWS::EC2::Volume" or (
-        configuration_item["configuration"]["volumeType"] != "gp2"
-    ):
-        return "NOT_APPLICABLE"
-
+    if configuration_item["configuration"]["volumeType"] == "gp2":
+        return "NON_COMPLIANT"
     if (
         configuration_item["configuration"]["volumeType"]
         == valid_rule_parameters["desiredvolumeType"]
     ):
         return "COMPLIANT"
 
-    return "NON_COMPLIANT"
+    return "NOT_APPLICABLE"
