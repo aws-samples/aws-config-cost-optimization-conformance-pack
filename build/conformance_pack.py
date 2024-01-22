978ae2870cf72b5a2a74c564453fdc8ff1f2fb96
Dan Johns <danjhd@amazon.com>
2024-01-22 09:24:49 +0000

    improve code readability

--- build/conformance_pack.py
+++ build/conformance_pack.py
@@ -15,45 +15,34 @@ files = glob.glob(f"{rules_folder}/[!_sample]**/resources.yaml", recursive=True)
 for resource_file in files:
     rule_name = resource_file.split("/")[-2]
     resource_content = load_yaml(open(resource_file, "r", encoding="utf-8").read())
-    resources = list(resource_content["Resources"].keys())
-    for resource in resources:
-        resource_content["Resources"][resource]["Properties"][
-            "ConfigRuleName"
-        ] = f"{rule_name_prefix}-{rule_name}"
-        if resource_content["Resources"][resource]["Type"] == "AWS::Config::ConfigRule":
-            resource_content["Resources"][resource]["Properties"]["Source"][
-                "SourceIdentifier"
-            ] = {
+    for logical_id, resource in resource_content["Resources"].items():
+        resource["Properties"]["ConfigRuleName"] = f"{rule_name_prefix}-{rule_name}"
+        if resource["Type"] == "AWS::Config::ConfigRule":
+            resource["Properties"]["Source"]["SourceIdentifier"] = {
                 "Fn::Sub": "arn:${AWS::Partition}:lambda:${AWS::Region}:${AWS::AccountId}:function:CostOptimizationConformanceConfigRuleFunction"
             }
-            if (
-                "InputParameters"
-                in resource_content["Resources"][resource]["Properties"]
-            ):
-                resource_content["Resources"][resource]["Properties"][
-                    "InputParameters"
-                ]["customFunctionPrefix"] = rule_name.replace("-", "_").lower()
+            if "InputParameters" in resource["Properties"]:
+                resource["Properties"]["InputParameters"][
+                    "customFunctionPrefix"
+                ] = rule_name.replace("-", "_").lower()
             else:
-                resource_content["Resources"][resource]["Properties"][
-                    "InputParameters"
-                ] = {"customFunctionPrefix": rule_name.replace("-", "_").lower()}
-        elif (
-            resource_content["Resources"][resource]["Type"]
-            == "AWS::Config::RemediationConfiguration"
-        ):
-            if "DependsOn" in resource_content["Resources"][resource]:
-                resource_content["Resources"][resource][
+                resource["Properties"]["InputParameters"] = {
+                    "customFunctionPrefix": rule_name.replace("-", "_").lower()
+                }
+        elif resource["Type"] == "AWS::Config::RemediationConfiguration":
+            if "DependsOn" in resource:
+                resource[
                     "DependsOn"
-                ] = f'{rule_name.replace("-", "")}{resource_content["Resources"][resource]["DependsOn"]}'
+                ] = f'{rule_name.replace("-", "")}{resource["DependsOn"]}'
             template["Parameters"][f"{rule_name.replace('-', '')}DocumentArn"] = {
                 "Type": "String"
             }
-            resource_content["Resources"][resource]["Properties"]["TargetId"] = {
+            resource["Properties"]["TargetId"] = {
                 "Ref": f"{rule_name.replace('-', '')}DocumentArn"
             }
         resource_content["Resources"][
-            f"{rule_name.replace('-', '')}{resource}"
-        ] = resource_content["Resources"].pop(resource)
+            f"{rule_name.replace('-', '')}{logical_id}"
+        ] = resource_content["Resources"].pop(logical_id)
     template["Resources"] = {**template["Resources"], **resource_content["Resources"]}
 
 open(output_path, "w", encoding="utf-8").write(dump_yaml(template))
